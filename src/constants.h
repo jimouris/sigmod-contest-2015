6ec847a68ef20d40b5487658dd0e01fe9952084c
Dimitris Mouris <jimmouris@hotmail.com>
2015-11-10 19:04:22 +0200

    add 2 more defines

--- src/constants.h
+++ src/constants.h
@@ -5,7 +5,8 @@
 #define B 1							/* Number of keys for each bucket */
 #define C 4							/* Initial capacity for transactions in bucket */
 #define JOURNAL_CAPACITY_INIT 64	
-
+#define OK_SUCCESS 0
+#define NOK_FAILURE 1
 
 #define ALLOCATION_ERROR(X) if (X == NULL) { \
 							perror("allocation error"); \
