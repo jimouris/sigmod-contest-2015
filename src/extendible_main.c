dd63c8c538f442a6ccdc533b78b640e96c46406b
Dimitris Mouris <jimmouris@hotmail.com>
2015-11-14 14:53:51 +0200

    relation 2 entries

--- src/extendible_main.c
+++ src/extendible_main.c
@@ -9,22 +9,52 @@ int main(void)
 	RangeArray **ranges = malloc(30 *sizeof(RangeArray *));
 	int i;
 	srand(time(NULL));
-	for (i = 0 ; i < 5 ; i++) {
+	for (i = 0 ; i < 16; i++) {
 		ranges[i] = malloc(sizeof(RangeArray));
 		ranges[i]->transaction_id = 20 + i;
 		ranges[i]->rec_offset = rand() % 200;
-		// if (i == 0)
-			insertHashRecord(myhash, i, ranges[i]);
+		if (i == 0)
+			insertHashRecord(myhash, 164, ranges[i]);
+		else if (i == 1)
+			insertHashRecord(myhash, 220, ranges[i]);
+		else if (i == 2)
+			insertHashRecord(myhash, 385, ranges[i]);
+		else if (i == 3)
+			insertHashRecord(myhash, 437, ranges[i]);
+		else if (i == 4)
+			insertHashRecord(myhash, 607, ranges[i]);
+		else if (i == 5)
+			insertHashRecord(myhash, 732, ranges[i]);
+		else if (i == 6)
+			insertHashRecord(myhash, 870, ranges[i]);
+		else if (i == 7)
+			insertHashRecord(myhash, 999, ranges[i]);
+		else if (i == 8)
+			insertHashRecord(myhash, 1010, ranges[i]);
+		else if (i == 9)
+			insertHashRecord(myhash, 1098, ranges[i]);
+		else if (i == 10)
+			insertHashRecord(myhash, 1154, ranges[i]);
+		else if (i == 11)
+			insertHashRecord(myhash, 1271, ranges[i]);
+		else if (i == 12)
+			insertHashRecord(myhash, 1427, ranges[i]);
+		else if (i == 13)
+			insertHashRecord(myhash, 1549, ranges[i]);
+		else if (i == 14)
+			insertHashRecord(myhash, 1607, ranges[i]);
+		else if (i == 15)
+			insertHashRecord(myhash, 1697, ranges[i]);
 		// else 
 			// insertHashRecord(myhash, 8, ranges[i]);
 	}
 
-	for (i = 5 ; i < 14 ;i++) {
-		ranges[i] = malloc(sizeof(RangeArray));
-		ranges[i]->transaction_id = 20 + i;
-		ranges[i]->rec_offset = rand() % 200;
-		insertHashRecord(myhash,16,ranges[i]);
-	}
+	// for (i = 5 ; i < 14 ;i++) {
+	// 	ranges[i] = malloc(sizeof(RangeArray));
+	// 	ranges[i]->transaction_id = 20 + i;
+	// 	ranges[i]->rec_offset = rand() % 200;
+	// 	insertHashRecord(myhash,8,ranges[i]);
+	// }
 
 	// r1->transaction_id = 800;
 	// r1->rec_offset = 43;
