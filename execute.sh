20b1e1ab6560691b87f7e9a09cb9db0d15ce5eeb
Dimitris Mouris <jimmouris@hotmail.com>
2015-12-15 15:58:58 +0200

    Update execute.sh to run with no arguments, tid, hash, both

--- execute.sh
+++ execute.sh
@@ -20,12 +20,22 @@ if [ $# -eq 0 ] ; then
 else
 	echo "reads from $1"
 	input="${1}"
-	if [ $# -eq 2 ] && [ "${2}" = "-tid" -o "${2}" = "--tid" -o "${2}" = "tid" ]
-		then
-		echo "tidHash is on"
+	if [ $# -eq 2 ] && [ "${2}" = "-tid" -o "${2}" = "--tid" -o "${2}" = "tid" ] ; then
+		echo "tidHash is enabled"
 		time -f "\t%E Elapsed Real Time \n\t%S CPU-seconds" ./a.out --tid < $input > myout.test
+	elif [ $# -eq 2 ] && [ "${2}" = "-predicate" -o "${2}" = "--predicate" -o "${2}" = "predicate" ] ; then
+		echo "predicateHash is enabled"
+		time -f "\t%E Elapsed Real Time \n\t%S CPU-seconds" ./a.out --predicate < $input > myout.test
+	elif [ $# -eq 3 ] ; then
+		if [ "${2}" = "-tid" -o "${2}" = "--tid" -o "${2}" = "tid" ] && [ "${3}" = "-predicate" -o "${3}" = "--predicate" -o "${3}" = "predicate" ] ; then
+			echo "Both predicateHash and tidHash is enabled"
+			time -f "\t%E Elapsed Real Time \n\t%S CPU-seconds" ./a.out --tid --predicate < $input > myout.test
+		elif  [ "${3}" = "-tid" -o "${3}" = "--tid" -o "${3}" = "tid" ] && [ "${2}" = "-predicate" -o "${2}" = "--predicate" -o "${2}" = "predicate" ] ; then
+			echo "Both predicateHash and tidHash is enabled"
+			time -f "\t%E Elapsed Real Time \n\t%S CPU-seconds" ./a.out --tid --predicate < $input > myout.test
+		fi
 	else
-		T="$(date +%s)"
+		# T="$(date +%s)"
 		time -f "\t%E Elapsed Real Time \n\t%S CPU-seconds" ./a.out < $input > myout.test
 	fi
 	./perlineprinter < myout.test > myoutperline.test
@@ -35,7 +45,16 @@ else
 	output=${output%.*}".out."$ending
 	# echo "$output"
 	./perlineprinter < $output > $output.perline.test
-	diff myoutperline.test $output.perline.test
+	# diff myoutperline.test $output.perline.test
+
+	DIFF=$(diff myoutperline.test $output.perline.test)
+	if [ "$DIFF" != "" ] ; then
+	    echo "\n\nThe force wasn't with you my friend"
+	    echo $DIFF
+	else
+	    echo "\n\nThe force was with you this time, diff num of results 0!!"
+	fi
+
 	DEFAULTOUTPUT=$output
 	echo "\n\n"
 fi
