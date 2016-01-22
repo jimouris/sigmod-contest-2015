b9f39e61c806eab59b5d14c1694efdf6f7b8a8a6
Dimitris Mouris <jimmouris@hotmail.com>
2016-01-22 16:14:56 +0200

    Create thread files

---
+++ src/scheduler.h
@@ -0,0 +1,13 @@
+#ifndef THREADFUNCS_H_
+#define THREADFUNCS_H_
+
+#include <stdio.h>
+#include <stdint.h>
+#include <pthread.h>
+#include <unistd.h>
+
+typedef struct thread_t {
+	
+} thread_t;
+
+#endif
