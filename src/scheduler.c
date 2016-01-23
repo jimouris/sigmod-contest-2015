538107284718e1b82804975cf54a10aa8f44b217
Dimitris Mouris <jimmouris@hotmail.com>
2016-01-23 11:17:35 +0200

    Add threadpoolCreate

--- src/scheduler.c
+++ src/scheduler.c
@@ -1,3 +1,18 @@
 #include "scheduler.h"
 
+threadpool_t* threadpoolCreate(int thread_count) {
+	threadpool_t* threadpool = malloc(sizeof(threadpool_t));
+	ALLOCATION_ERROR(threadpool);
+	threadpool->threads = malloc(thread_count * sizeof(pthread_t));
+	ALLOCATION_ERROR(threadpool->threads);
+	if (pthread_mutex_init(&(threadpool->lock), NULL) != 0) {
+		fprintf(stderr, "Mutex allocation Error\n");
+		exit(EXIT_FAILURE);
+	}
+	threadpool->thread_count = thread_count;
+
+	return threadpool;
+}
+
+
 
--- src/scheduler.h
+++ src/scheduler.h
@@ -6,6 +6,7 @@
 #include <pthread.h>
 #include <unistd.h>
 #include "parser.h"
+#include "constants.h"
 
 typedef struct thread_arg_t {
 	ValidationQueries_t** validation_array;
@@ -15,5 +16,11 @@ typedef struct thread_arg_t {
 	Journal_t** journal_array;
 	uint64_t first_val_id;
 } thread_arg_t;
+
+typedef struct threadpool_t {
+	pthread_t *threads;
+	int thread_count;
+	pthread_mutex_t lock;
+} threadpool_t;
 
 #endif
